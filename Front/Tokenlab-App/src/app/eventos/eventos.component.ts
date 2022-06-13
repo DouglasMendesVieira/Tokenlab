b297eeef556ed057b1f1c6ce20c6dc5842b3015f
Douglas Mendes <douglas.mendes.vieira@gmail.com>
2022-06-13 17:02:09 -0300

    Adicionando a Listagem de Eventos

--- Front/Tokenlab-App/src/app/eventos/eventos.component.ts
+++ Front/Tokenlab-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos:any;
+  public eventos:any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg = 160;
+  marginImg = 2;
+  showImg = true;
+  private _filterList = '';
+
+  public get filterList() {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.eventosFiltrados = this.filterList ? this.filtrarEventos(this.filterList) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  changeButtonImg() {
+    this.showImg = !this.showImg
+  }
+
   public getEventos():void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      responde => this.eventos = responde,
+      responde => {
+        this.eventos = responde,
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
